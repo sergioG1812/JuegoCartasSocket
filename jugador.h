abfd522eba5d7367fad2b34f110da0a7d918cd87
sergioG1812 <sergioa.gonzaleza@outlook.com>
2025-05-06 23:27:31 -0500

    cambios minimos

--- jugador.h
+++ jugador.h
@@ -10,25 +10,20 @@ class Jugador {
 public:
     explicit Jugador(const string& nombre);
 
-    // Marca local/remoto
     void setLocal(bool esLocal);
     bool esJugadorLocal() const;
 
-    // Para el reparto: limpia estado previo
     void vaciarMano();
     void vaciarCartasGanadas();
     void agregarCarta(const Carta& c);
 
-    // Lógica de juego
     Carta jugarCarta();
     int   getUltimaCartaJugadaIndice() const;
 
-    // Consultas
     int   getNumeroCartas() const;
     Carta getCartaEnMano(int idx) const;
     void  quitarCarta(int idx);
 
-    // Cartas ganadas
     void agregarCartaGanada(const Carta& c);
     int  getCartasGanadas() const;
 
