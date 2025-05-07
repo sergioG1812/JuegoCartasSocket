108126b5a9a1cd2d047b3757f00d4cf8dee73899
sergioG1812 <sergioa.gonzaleza@outlook.com>
2025-05-06 23:35:08 -0500

    cliente.cpp
    Ingreso del namespace std

--- client.cpp
+++ client.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 #include <winsock2.h>
 #include "Juego.h"
 #pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
 
 #define PORT      8080
 #define SERVER_IP "192.168.1.9"
@@ -9,23 +13,24 @@
 int main() {
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        std::cerr << "WinSock init failed: " << WSAGetLastError() << "\n";
+        cerr << "WinSock init failed: " << WSAGetLastError() << "\n";
         return 1;
     }
 
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in serv{};
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons(PORT);
-    serv.sin_addr.s_addr = inet_addr(SERVER_IP);  // IP fija
+    serv.sin_family      = AF_INET;
+    serv.sin_port        = htons(PORT);
+    serv.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     if (connect(sock, (sockaddr*)&serv, sizeof(serv)) == SOCKET_ERROR) {
-        std::cerr << "Connect failed: " << WSAGetLastError() << "\n";
+        cerr << "Connect failed: " << WSAGetLastError() << "\n";
+        WSACleanup();
         return 1;
     }
-    std::cout << "Conectado al servidor.\n";
+    cout << "Conectado al servidor.\n";
 
-    std::vector<std::string> nombres = { "Servidor", "Cliente" };
+    vector<string> nombres = { "Servidor", "Cliente" };
     Juego juego(nombres);
     juego.jugarSocketClient(sock);
 
