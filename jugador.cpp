d3a1294f39403f55ef35883d2f374e657b7d8f12
sergioG1812 <sergioa.gonzaleza@outlook.com>
2025-05-08 16:28:42 -0500

    jugador.cpp

--- jugador.cpp
+++ jugador.cpp
@@ -1,11 +1,12 @@
-﻿#include "Jugador.h"
+#include "Jugador.h"
 #include <iostream>
 #include <limits>
 
 using namespace std;
 
 Jugador::Jugador(const string& nombre_)
-  : nombre(nombre_), local(true), ultimaJugadaIdx(-1) {}
+    : nombre(nombre_), local(true), ultimaJugadaIdx(-1) {
+}
 
 void Jugador::setLocal(bool esLocal) {
     local = esLocal;
@@ -17,60 +18,25 @@ bool Jugador::esJugadorLocal() const {
 
 void Jugador::vaciarMano() {
     mano.clear();
-    ultimaJugadaIdx = -1;
-}
-
-void Jugador::vaciarCartasGanadas() {
-    cartasGanadas.clear();
 }
 
 void Jugador::agregarCarta(const Carta& c) {
     mano.push_back(c);
 }
 
-Carta Jugador::jugarCarta() {
-    if (mano.empty())
-        throw out_of_range("No hay cartas para jugar");
-
-    cout << "Mano de " << nombre << ":\n";
-    for (int i = 0; i < (int)mano.size(); ++i) {
-        cout << (i+1) << ": ";
-        mano[i].mostrar();
-        cout << "\n";
-    }
-
-    int opcion;
-    cout << "Elige numero de carta [1-" << mano.size() << "]: ";
-    while (!(cin >> opcion) || opcion < 1 || opcion > (int)mano.size()) {
-        cout << "Indice inválido. Intenta de nuevo: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-
-    int idx = opcion - 1;
-    ultimaJugadaIdx = idx;
-    Carta c = mano[idx];
-    mano.erase(mano.begin() + idx);
-    return c;
-}
-
-int Jugador::getUltimaCartaJugadaIndice() const {
-    return ultimaJugadaIdx;
-}
-
 int Jugador::getNumeroCartas() const {
-    return (int)mano.size();
+    return static_cast<int>(mano.size());
 }
 
 Carta Jugador::getCartaEnMano(int idx) const {
-    if (idx < 0 || idx >= (int)mano.size())
-        throw out_of_range("Indice de carta fuera de rango");
+    if (idx < 0 || idx >= static_cast<int>(mano.size()))
+        throw out_of_range("Índice de carta fuera de rango");
     return mano[idx];
 }
 
 void Jugador::quitarCarta(int idx) {
-    if (idx < 0 || idx >= (int)mano.size())
-        throw out_of_range("Indice de carta fuera de rango");
+    if (idx < 0 || idx >= static_cast<int>(mano.size()))
+        throw out_of_range("Índice de carta fuera de rango");
     ultimaJugadaIdx = idx;
     mano.erase(mano.begin() + idx);
 }
@@ -80,5 +46,5 @@ void Jugador::agregarCartaGanada(const Carta& c) {
 }
 
 int Jugador::getCartasGanadas() const {
-    return (int)cartasGanadas.size();
+    return static_cast<int>(cartasGanadas.size());
 }
